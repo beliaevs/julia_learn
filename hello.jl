a8ef8be8cdb4900157131ad8a086f576114f9b44
Beliaev <beliaevs@gmail.com>
2022-09-07 00:36:51 +0300

    parametric types

--- hello.jl
+++ hello.jl
@@ -11,6 +11,16 @@ function modify_arg(p::Point)
     p.y += 2
 end
 
+struct Vec{T}
+    x::T
+    y::T
+end
+
+v1 = Vec{Int}(1, 2)
+v2 = Vec{Int}(3, 4)
+v3 = v1 + v2
+println("v3.x=", v3.x, ", v3.y=", v3.y)
+
 p = Point(100, 200)
 modify_arg(p)
 println("p.x = ", p.x, ", p.y = ", p.y)
